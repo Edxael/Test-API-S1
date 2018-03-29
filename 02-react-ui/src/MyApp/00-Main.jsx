a77f52db490b1de72b25f9ec6441d153730d4091
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-29 00:41:18 -0600

    start reactjs

--- 02-react-ui/src/MyApp/00-Main.jsx
+++ 02-react-ui/src/MyApp/00-Main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+//   https://github.com/Edxael/Gral-02-18/blob/master/02-React/20-R-API-1/src/MyApp/01-Second.jsx
 
 export default class extends React.Component{
     render(){
-        console.log("Leones")
+
+        const getAll = () => {
+            console.log("Get all the singers")
+        }
+
         return(
             <div>
                 <h1>SINGERS CRUD-API</h1>
+                <hr/>
+
+                <button onClick={getAll} >Get All singers</button>
             </div>
         )
     }
