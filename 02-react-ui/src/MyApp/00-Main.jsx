1afe75bfe5302e2c8c6cf69a38e33ff62330823f
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-29 12:59:42 -0600

    add style

--- 02-react-ui/src/MyApp/00-Main.jsx
+++ 02-react-ui/src/MyApp/00-Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import './style.css'
 //   https://github.com/Edxael/Gral-02-18/blob/master/02-React/20-R-API-1/src/MyApp/01-Second.jsx
 
 export default class extends React.Component{
@@ -11,9 +12,7 @@ export default class extends React.Component{
             let allRecords = await axios.get('http://localhost:5000/api/V1/singers')
                 .catch((err) => { return err })
 
-            console.log("The Result: ", allRecords)
-
-
+            console.log("The Result: ", allRecords.data)
         }
 
         return(
@@ -21,7 +20,14 @@ export default class extends React.Component{
                 <h1>SINGERS CRUD-API</h1>
                 <hr/>
 
-                <button onClick={getAll} >Get All singers.</button>
+                <div className="section" >
+                    <button onClick={getAll} >Get All singers.</button>
+                </div>
+
+                <div className="section" >
+                    <button>Get One By ID</button>
+                </div>
+
             </div>
         )
     }
